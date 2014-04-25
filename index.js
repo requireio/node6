70905444243150b321e301a97311a8d7e4478fbf
Hugh Kennedy <hughskennedy@gmail.com>
2014-04-25 17:02:23 +0200

    Move source transforms into separate files

--- index.js
+++ index.js
@@ -2,40 +2,16 @@
 
 
 var ModuleMap = require('module-map')
-var transpile = require('./transpile')
-var Defs = require('defs')
-var esNext = require('esnext')
-var harmonyParse = require("esprima-fb").parse
-var Destructuring = require('es6-destructuring-jstransform');
 
+var destructuring = require('./lib/destructuring')
+var polyfills = require('./lib/polyfills')
+var esnext = require('./lib/esnext')
+var defs = require('./lib/defs')
 
 module.exports = function(dir) {
   var map = ModuleMap(dir)
+  map(polyfills)
   map(destructuring)
   map(defs)
   map(esnext)
-}
-
-function esnext(content, filename) {
-  var e = esNext.compile(content, {
-    includeRuntime: true
-  })
-  return e.code
-}
-
-function defs(content, filename, options) {
-  var d = Defs(content, {
-    "environments": ["node"],
-    "loopClosures": "iife",
-    "disallowVars": false,
-    "disallowDuplicated": false,
-    "disallowUnknownReferences": false,
-    parse: harmonyParse
-  })
-  return d.src
-}
-
-function destructuring(content) {
-  var d = Destructuring(content)
-  return d.code
 }
